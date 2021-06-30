71e9ed7d29fc7a3db7e54318c28277700f15b1bc
Kim Sung Hyun <soakdma37@gmail.com>
2021-06-30 21:04:10 +0900

    first commit in react board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {CKEditor} from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>영화 리뷰</h1>
+      <div className='movie-container'>
+        <h2>제목</h2>
+        <div>
+          내용
+        </div>
+      </div>
+
+      <div className='form-wrapper'>
+        <input className="title-input" type='text' placeholder='제목'/>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor5</p>"
+          onReady={editor=>{
+            console.log('editor is ready to use', editor);
+          }}
+          onChange={(event,editor)=>{
+            const data=editor.getData();
+            console.log({event, editor, data});
+          }}
+          onBlur={(event, editor)=>{
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor)=>{
+            console.log('Focus.',editor);
+          }}
+          />
+      </div>
+      <button className="submit-button">입력</button>
     </div>
   );
 }

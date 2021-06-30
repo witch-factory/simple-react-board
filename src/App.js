9313d8de6d1beb51577e3d6bed1df31a2ff15b7d
Kim Sung Hyun <soakdma37@gmail.com>
2021-06-30 21:30:49 +0900

    동적으로 글 리스트 보여주기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {useState} from 'react';
 
 
 function App() {
+  const [movieContent, setMovieContent]=useState({
+    title:'',
+    content:''
+  })
+
+  const getValue=(e)=>{
+    const {name, value}=e.target;
+    setMovieContent({
+      ...movieContent,
+      [name]:value,
+    })
+    console.log(movieContent);
+  }
+
+  const [viewContent, setViewContent]=useState([]);
+
   return (
     <div className="App">
       <h1>영화 리뷰</h1>
       <div className='movie-container'>
-        <h2>제목</h2>
-        <div>
-          내용
-        </div>
+        {viewContent.map(content=>(
+          <div key={content.title}>
+            <h2>{content.title}</h2>
+            <div>
+              {content.content}
+            </div>
+          </div>
+          )
+        )}
       </div>
 
       <div className='form-wrapper'>
-        <input className="title-input" type='text' placeholder='제목'/>
+        <input className="title-input" type='text' placeholder='제목' onChange={getValue} name='title' />
         <CKEditor
           editor={ClassicEditor}
           data="<p>Hello from CKEditor5</p>"
@@ -25,6 +47,11 @@ function App() {
           onChange={(event,editor)=>{
             const data=editor.getData();
             console.log({event, editor, data});
+            setMovieContent({
+              ...movieContent,
+              content:data
+            })
+            console.log(movieContent);
           }}
           onBlur={(event, editor)=>{
             console.log('Blur.', editor);
@@ -34,7 +61,12 @@ function App() {
           }}
           />
       </div>
-      <button className="submit-button">입력</button>
+      <button
+        className="submit-button"
+        onClick={()=>{
+        setViewContent(viewContent.concat({...movieContent}))}
+        }
+      >입력</button>
     </div>
   );
 }

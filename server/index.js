a71a54766bdc407225ed0245d6943ec65622489a
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-02 21:19:43 +0900

    게시판 튜토리얼 3 진행중

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const PORT=process.env.port || 8000;
 const mysql=require('mysql');
+const cors=require('cors');
 
 const bodyParser=require('body-parser');
 const {urlencoded}=require('body-parser');
@@ -13,14 +14,26 @@ const db=mysql.createPool({
   database:"simpleboard"
 });
 
-app.get("/", (req,res)=>{
-  const sqlQuery= "INSERT INTO simpleboard(title, content) values('내가 본 영화', '좋은 영화였다.')";
-  db.query(sqlQuery, (err, result)=>{
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res)=>{
+  const sqlQuery="select * from simpleboard";
+  db.query(sqlQuery, (err,result)=>{
+    res.send(result);
+  })
+});
+
+app.post("/api/insert", (req, res)=>{
+  const title=req.body.title;
+  const content=req.body.content;
+  const sqlQuery = "INSERT INTO simpleboard (title, content) VALUES (?,?)";
+  db.query(sqlQuery, [title, content], (err, result)=>{
     res.send('success!');
   });
-})
+});
 
-console.log(`${PORT}`);
 
 app.listen(PORT, ()=>{
   console.log(`running on port ${PORT}`);
